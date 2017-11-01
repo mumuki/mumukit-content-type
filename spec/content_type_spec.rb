7f6e18899abc00fdff250177bf22e07f42b6b08f
Julian Berbel Alt <julianberbel@hotmail.com>
2017-11-01 12:35:20 -0300

    Add new tab markdown link test

--- spec/content_type_spec.rb
+++ spec/content_type_spec.rb
@@ -22,6 +22,7 @@ describe Mumukit::ContentType do
     it { expect(markdown.to_html("\n| f | x |\n|---|---|\n| x | y |\n")).to eq "<table class=\"table\"><thead>\n<tr>\n<th>f</th>\n<th>x</th>\n</tr>\n</thead><tbody>\n<tr>\n<td>x</td>\n<td>y</td>\n</tr>\n</tbody></table>\n" }
     it { expect(markdown.to_html('ム')).to eq "<p><i class=\"text-primary da da-mumuki\"></i></p>\n" }
     it { expect(markdown.to_html("ム foo\nム bar\n")).to_not include "ム" }
+    it { expect(markdown.to_html('[this is a link somewhere with a title](http://www.somewhere.com "title here")')).to eq "<p><a title=\"title here\" href=\"http://www.somewhere.com\" target=\"_blank\">this is a link somewhere with a title</a></p>\n"}
   end
 
   describe 'highlighted_code' do
