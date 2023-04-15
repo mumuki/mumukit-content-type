6b09a85a3f0487b5364357a6d70e7e6e410bcb51
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-04-15 01:54:43 -0300

    Ensure sanitizer works with rails 6.1+

--- lib/mumukit/content_type/sanitizer.rb
+++ lib/mumukit/content_type/sanitizer.rb
@@ -1,16 +1,20 @@
 require 'sanitize'
 
 module Mumukit::ContentType::Sanitizer
+  mattr_accessor :should_sanitize, :allowed_elements, :allowed_attributes, :transformers
+
+  self.should_sanitize = false
+  self.allowed_elements = []
+  self.allowed_attributes = {
+      'a' => Sanitize::Config::RELAXED[:attributes]['a'] + ['target']
+  }
+
+  self.transformers = []
+
   class << self
-    class_attribute :should_sanitize, :allowed_elements, :allowed_attributes, :transformers
-
-    self.should_sanitize = false
-    self.allowed_elements = []
-    self.allowed_attributes = {
-        'a' => Sanitize::Config::RELAXED[:attributes]['a'] + ['target']
-    }
-
-    self.transformers = []
+    def should_sanitize?
+      should_sanitize
+    end
 
     def sanitize(html)
       return html unless should_sanitize?
